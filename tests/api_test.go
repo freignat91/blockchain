4e4f05855b68c39558dd2bffbb2ad6378d06fd67
freignat <freignat@axway.com>
2017-03-04 11:16:38 +0100

    update tests

--- tests/api_test.go
+++ tests/api_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/freignat91/blockchain/api"
 	"os"
 	"testing"
+	"time"
 )
 
 const (
@@ -18,6 +19,72 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+//time.Sleep(1*time.Second) need on v0.0.1, will be deleted on v0.0.2
+
 func TestReady(t *testing.T) {
-	bcapi.GetNbReady(3)
+	if err := bcapi.UserSignup("test", "/tmp/blockchain/private.key"); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	if err := bcapi.SetUser("test", "/tmp/blockchain/private.key"); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	if err := bcapi.AddBranch([]string{"organization=company1"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddBranch([]string{"organization=company1", "member=member11"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddBranch([]string{"organization=company1", "member=member12"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddBranch([]string{"organization=company2"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddBranch([]string{"organization=company2", "member=member21"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddBranch([]string{"organization=company2", "member=member22"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddEntry([]byte("doc111"), []string{"organization=company1", "member=member11"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddEntry([]byte("doc112"), []string{"organization=company1", "member=member11"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddEntry([]byte("doc113"), []string{"organization=company1", "member=member11"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddEntry([]byte("doc114"), []string{"organization=company1", "member=member11"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddEntry([]byte("doc115"), []string{"organization=company1", "member=member11"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddEntry([]byte("doc116"), []string{"organization=company1", "member=member11"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddEntry([]byte("doc117"), []string{"organization=company1", "member=member11"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddEntry([]byte("doc118"), []string{"organization=company1", "member=member11"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	if err := bcapi.AddEntry([]byte("doc119"), []string{"organization=company1", "member=member11"}); err != nil {
+		t.Fatalf("error: %v\n", err)
+	}
 }
